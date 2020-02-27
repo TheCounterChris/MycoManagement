08aa913897993532a69b7dcb46c438da2457c4e2
adamccaffrey <adam_mccaffrey@hotmail.co.uk>
2020-02-27 15:46:26 +0000

    addition of mushroom incubator scripts

--- MycoManagement Game Project/Assets/Scripts/CharController.cs
+++ MycoManagement Game Project/Assets/Scripts/CharController.cs
@@ -44,13 +44,13 @@ public class CharController : MonoBehaviour
         Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
         Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
         Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
-        Debug.Log("HEADING" + heading);
+        // Debug.Log("HEADING" + heading);
         transform.forward = heading;
         transform.position += rightMovement;
         transform.position += upMovement;
         velocity = moveSpeed * Mathf.Clamp(Mathf.Max(Mathf.Abs(heading.x), Mathf.Abs(heading.z)), 0f, 0.7f);
 
-        Debug.Log("velocity" + velocity);
+        // Debug.Log("velocity" + velocity);
         // pdPatch.SendFloat("velocity", Mathf.Clamp(velocity, 0, 1));
     }
 }
---
+++ MycoManagement Game Project/Assets/Scripts/SubstrateToIncubator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubstrateToIncubator : MonoBehaviour
+
+{
+
+    MushroomManager shroom;
+
+    string shroomName;
+
+    void Start() {
+        shroom = FindObjectOfType<MushroomManager>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Fungi"))
+        {
+            Debug.Log("collision detected");
+            shroomName = other.name;
+            shroom.AddMushroom(shroomName);
+        }
+    }
+}
+
