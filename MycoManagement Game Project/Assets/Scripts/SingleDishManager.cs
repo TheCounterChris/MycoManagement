1ad75d8c424e1c3e7b3625cb670a43ee6e156b2d
Chrxstian <Chrxstian@users.noreply.github.com>
2020-02-29 14:08:01 +0000

    dish gets filled lol

---
+++ MycoManagement Game Project/Assets/Scripts/SingleDishManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleDishManager : MonoBehaviour
+{
+    GameObject emptyDish;//empty petri dish
+    GameObject fullDish;//filled petri dish
+    GameObject grownDish;//petri dish with mycelium grown
+
+    GameObject gills;// gills
+
+    void Start() 
+    {
+        emptyDish = gameObject.transform.GetChild(0).gameObject;
+        fullDish = gameObject.transform.GetChild(1).gameObject;
+        grownDish = gameObject.transform.GetChild(2).gameObject;
+    }
+
+    public void fillDish()//put the gills in the dish
+    {
+        emptyDish.SetActive(false);//turn off empty dish
+        fullDish.SetActive(true);//turn on dish with gills
+
+        //gills.SetActive(false);
+    }
+
+    public void GrowDish()
+    {
+        fullDish.SetActive(false);//turn off dish with gills
+        grownDish.SetActive(true);//turn on dish with gills and mycelium
+        //grownDish.transform.parent = this.transform;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Gills")
+        {
+            fillDish();
+            Destroy(other.gameObject);
+        }
+    }
+}
