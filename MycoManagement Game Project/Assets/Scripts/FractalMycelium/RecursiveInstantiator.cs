5a70bce053d579c68352777bf5352c69c52b86d8
alastairpeat <ali_peat_01@hotmail.co.uk>
2020-03-10 19:54:10 +0000

    \

---
+++ MycoManagement Game Project/Assets/Scripts/FractalMycelium/RecursiveInstantiator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecursiveInstantiator : MonoBehaviour {
+
+    public int recurse = 5;
+    public int splitNumber = 2;
+    public Vector3 pivotPosition;
+
+	// Use this for initialization
+	void Start () {
+        recurse -= 1;
+
+	    for (int i = 0; i < splitNumber; ++i)
+	    {
+	        if (recurse > 0)
+	        {
+	            var copy = Instantiate(gameObject);
+	            var recursive = copy.GetComponent<RecursiveInstantiator>();
+	            recursive.SendMessage("Generated", new RecursiveBundle() { Index = i, Parent = this });
+	        }
+	    }
+	}
+	void Update() {
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				recurse += 1;
+				 for (int i = 0; i < splitNumber; ++i)
+	    {
+	       
+	        {
+	            var copy = Instantiate(gameObject);
+	            var recursive = copy.GetComponent<RecursiveInstantiator>();
+	            recursive.SendMessage("Generated", new RecursiveBundle() { Index = i, Parent = this });
+	        }
+	    }
+			}
+		}
+	}
+	}
+
