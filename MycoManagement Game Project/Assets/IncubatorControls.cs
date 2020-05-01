c9328453d94c47c0eedfc1600eb6b782224eaba0
adamccaffrey <adam_mccaffrey@hotmail.co.uk>
2020-05-01 17:41:33 +0100

    incubator

---
+++ MycoManagement Game Project/Assets/IncubatorControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IncubatorControls : MonoBehaviour
+{
+
+    public Slider incubatorTempSlider;
+
+    public Slider incubatorHumiditySlider;
+
+    public float incubatorTemp;
+
+    public float incubatorHumidity;
+    void Start() 
+    {
+        incubatorTemp = incubatorTempSlider.value;
+        incubatorHumidity = incubatorHumiditySlider.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
