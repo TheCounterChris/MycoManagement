f1026e3ed7cd73f4d97525d040c51ad8b9fdec40
Chrxstian <TheCounterChris@users.noreply.github.com>
2020-05-02 20:32:19 -0400

    Grow Room beginning to be set up

--- MycoManagement Game Project/Assets/Scripts/SubstrateToIncubator.cs
+++ MycoManagement Game Project/Assets/Scripts/SubstrateToIncubator.cs
@@ -8,9 +8,61 @@ public class SubstrateToIncubator : MonoBehaviour
     MushroomManager2 shroom;
     string shroomName;
 
+    public List<GameObject> incSlots = new List<GameObject>();
+    GameObject availableSlot;
+
+    GameObject subBag;
+
+    bool playerReady = false;
+    bool bagReady = false;
+
+    public GameObject thirdPersonHoldPos;
+
     void Start()
     {
         shroom = FindObjectOfType<MushroomManager2>();
+    }
+
+    void Update()
+    {
+        if(bagReady == true)
+        {
+            if(Input.GetKeyDown("r"))
+            {
+                checkAvailability();
+                subBag.gameObject.transform.position = availableSlot.transform.position;
+                subBag.gameObject.transform.parent = availableSlot.transform;
+                //subBag.gameObject.transform.rotation = Quaternion.identity;
+                subBag.gameObject.GetComponent<PickUp>().enabled = false;
+                bagReady = false;
+            }
+        }
+
+        if(playerReady)
+        {
+            if(thirdPersonHoldPos.transform.childCount == 0)
+            {
+                Debug.Log("Press T");
+                if(Input.GetKeyDown("t"))
+                {
+                    Debug.Log("key pressed");
+                    for(int i = 0; i < incSlots.Count; i++)
+                    {
+                        Debug.Log("recursive process");
+                        if(incSlots[i].transform.childCount > 0)
+                        {
+                            GameObject firstBag = incSlots[i].transform.GetChild(0).gameObject;
+                            firstBag.transform.position = thirdPersonHoldPos.transform.position;
+                            firstBag.transform.parent = thirdPersonHoldPos.transform;
+                            firstBag.gameObject.GetComponent<PickUp>().enabled = true;
+                            Debug.Log("picked up object");
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -19,6 +71,17 @@ public class SubstrateToIncubator : MonoBehaviour
         {
             shroomName = other.name;
             shroom.AddMushroom(shroomName);
+
+            subBag = other.gameObject;
+
+            bagReady = true;
+
+            checkAvailability();
+        }
+
+        if(other.tag.Equals("Player"))
+        {
+            playerReady = true;
         }
     }
 
@@ -28,8 +91,27 @@ public class SubstrateToIncubator : MonoBehaviour
         {
             shroomName = other.name;
             shroom.StopMushroom(shroomName);
+
+            bagReady = false;
         }
-        // other.gameObject.tag = "Fungi";
+
+
+        if(other.tag.Equals("Player"))
+        {
+            playerReady = false;
+        }
+    }
+
+    void checkAvailability()
+    {
+        for(int i = 0; i < incSlots.Count; i++)
+        {
+            if(incSlots[i].transform.childCount < 1)
+            {
+                availableSlot = incSlots[i];
+                break;
+            }
+        }
     }
 }
 
