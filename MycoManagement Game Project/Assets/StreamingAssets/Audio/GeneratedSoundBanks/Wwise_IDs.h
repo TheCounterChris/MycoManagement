df2b7baa20bedf82fd562fef283d40cb7956c246
adamccaffrey <adam_mccaffrey@hotmail.co.uk>
2020-05-04 14:47:28 +0100

    sounds

--- MycoManagement Game Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ MycoManagement Game Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,10 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID GAMEMUSICLOOP = 820567678U;
+        static const AkUniqueID IMPACT = 3257506471U;
         static const AkUniqueID PICKUP = 3978245845U;
+        static const AkUniqueID SHROOMGROWTH = 2997160352U;
     } // namespace EVENTS
 
     namespace BANKS
@@ -25,7 +28,13 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID RVB = 695384145U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID RVB_BUS = 3637567782U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
